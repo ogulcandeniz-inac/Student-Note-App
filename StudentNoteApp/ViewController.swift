8b99c0de4b76307bd89d9665972f1939e82959a5
ogulcandeniz-inac <109241786+ogulcandeniz-inac@users.noreply.github.com>
2023-05-10 18:03:21 -0700

    Initial Commit

---
+++ StudentNoteApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StudentNoteApp
+//
+//  Created by Oğulcan Deniz İnaç on 10.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
